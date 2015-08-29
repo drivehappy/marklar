6f430e057574061796e92603fd277bd815426f5b
drivehappy <drivehappy@gmail.com>
2015-08-29 07:58:50 -0700

    Switched build to CMake, things are still in flux here

---
+++ src/libmarklarc/CMakeFiles/libmarklarc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libmarklarc.dir/codegen.cpp.o"
+  "CMakeFiles/libmarklarc.dir/parser.cpp.o"
+  "CMakeFiles/libmarklarc.dir/driver.cpp.o"
+  "liblibmarklarc.pdb"
+  "liblibmarklarc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libmarklarc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
